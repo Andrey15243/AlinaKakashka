c83aae5b937ce721939b2c1c23ded428b1c5d1b8
Andrey15243 <freeskater91@inbox.ru>
2025-01-05 17:04:12 +0300

    scropt.js

--- scropt.js
+++ scropt.js
@@ -1,15 +1,20 @@
 function askQuestion() {
     let userInput = prompt("Алина какашка?");
-  
+    
     if (userInput === null) {
       alert("Подумай ещё");
+      askQuestion();
+      return;
+    }
+
+    if (userInput.trim() === "") {
       askQuestion();
       return;
     }
   
     userInput = userInput.trim().toLowerCase();
   
-    if (userInput === "да" || userInput === "") {
+    if (userInput === "да") {
       alert("Верно!");
     } else {
       alert("Подумай ещё");
